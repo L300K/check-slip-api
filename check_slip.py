ab6ae66080b678c717770b238b7080e03437ea0e
L3ook <ss.sawarach@gmail.com>
2025-05-16 18:17:39 +0700

    feat: remove unnecessary parameter

--- check_slip.py
+++ check_slip.py
@@ -16,25 +16,9 @@ class checkSlip:
         self.php_sess_id = None
         self.ts = None
         self.slip_data = None
-        self.headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            'priority': 'u=0, i',
-            'referer': 'https://checkslip.scb.co.th/web/main',
-            'sec-ch-ua': '"Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36'
-        }
 
     def get_captcha(self):
-        response = requests.get('https://checkslip.scb.co.th/web/main', headers=self.headers)
+        response = requests.get('https://checkslip.scb.co.th/web/main')
         self.ts = requests.utils.dict_from_cookiejar(response.cookies)['TS010b4e86']
         cookies = {
             'TS010b4e86': self.ts
@@ -101,8 +85,7 @@ class checkSlip:
             response = requests.post(
                 'https://checkslip.scb.co.th/web/main',
                 cookies=cookies,
-                data=data,
-                headers=self.headers
+                data=data
             )
 
             system_err = "System is unavailable. Please try again"
